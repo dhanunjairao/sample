f1dee7d5a1de68c77ca9b59a05695de9bdff75fc
dhanunjairao <dhanunjairao_ari@srmap.edu.in>
2026-03-06 16:58:24 +0530

    1st

--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        public class Main {
-    public static void main(String[] args) {
+    
+            
 
-        System.out.println("Name: Dhanunjai Rao");
-        System.out.println("Age: 20");
-        System.out.println("Course: Computer Science");
-        System.out.println("College: SRM University");
-        System.out.println("City: Amaravati");
-
+                System.out.println("Age: 20");
+                System.out.println("Course: Computer Science and Engineering");
+                System.out.println("College: SRM University");
+                System.out.println("City: Amaravati");
+                
     }
-}
+    }
+            
         
-    }
-}
+
+    

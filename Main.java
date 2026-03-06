5df52d17bb21f89991ee14a3ca4c4c7df989831f
dhanunjairao <dhanunjairao_ari@srmap.edu.in>
2026-03-06 16:33:20 +0530

    sample

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+        do {
+            System.out.println("Say Hello");
+        } while (false);
+        while (n <= 5) {
+            System.out.print(n + " ");
+            n++;
+        }
+        System.out.println();
+        System.out.println("Program Exited");
+    }
+}
